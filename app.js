0c3e0549ac71ba560cc3513d8389250e254a5539
Hamdi Hijazi <82705892+hamdiZH@users.noreply.github.com>
2023-02-21 14:39:50 +0200

    swagger docs added

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -29,11 +35,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
 
 app.get('/', (req, res) => {
-  res.send('jobs api')
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
